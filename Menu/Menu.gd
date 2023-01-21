fad442dda1fcdb02e19efec88780422352f9d8b8
evanlu1134 <evanlu1134@gmail.com>
2023-01-21 18:07:25 -0500

    added the menu scene

---
+++ Menu/Menu.gd
@@ -0,0 +1,28 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$ColorRect/VBoxContainer/Start.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Start_pressed():
+	get_tree().change_scene("res://kitchen/kitchen.tscn");
+
+#func _on_Options_pressed():
+#	var options = load().instance()
+#	get_tree().current_scene.add_child(options)
+
+func _on_Quit_pressed():
+	get_tree().quit()
+
