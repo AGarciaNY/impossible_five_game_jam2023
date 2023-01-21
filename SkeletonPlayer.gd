7659262daaa0598cdb2e316b780693ea9ca8294d
Jason Alvarez <jasonalvarezcoraiza@gmail.com>
2023-01-21 13:13:17 -0500

    player movement and crouch animation

--- SkeletonPlayer.gd
+++ SkeletonPlayer.gd
@@ -5,6 +5,7 @@ const MAX_SPEED = 80
 const FRICTION = 1000
 var velocity = Vector2.ZERO
 var isSprinting = false
+var isCrouching = false
 var money = 0
 onready var animationPlayer = $AnimationPlayer
 
@@ -21,8 +22,14 @@ func _physics_process(delta):
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
-#	Input.is_action_pressed()
-	if input_vector != Vector2.ZERO:
+
+	#sprinting logic
+	isSprinting = Input.is_action_pressed("ui_sprint")
+	isCrouching = Input.is_action_pressed("ui_crouch")
+
+	if isCrouching:
+		state = CROUCH
+	elif input_vector != Vector2.ZERO:
 		state = WALK
 	else:
 		state = STAY
@@ -31,11 +38,21 @@ func _physics_process(delta):
 		STAY:
 			animationPlayer.play("Idle")
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		CROUCH:
+			if Input.is_action_just_released("ui_sprint"):
+				isSprinting = false
+			if Input.is_action_just_pressed("ui_crouch"):
+				animationPlayer.play("Crouch")
+			if Input.is_action_just_released("ui_crouch"):
+				isCrouching = false
+			
+			if isSprinting:
+				velocity = velocity.move_toward(input_vector * MAX_SPEED * .8, ACCELERATION * delta)
+			else:
+				velocity = velocity.move_toward(input_vector * MAX_SPEED * .5, ACCELERATION * delta)
 		WALK:
 			#animation logic
 			animationPlayer.play("Walk")
-			#sprinting logic
-			isSprinting = Input.is_action_pressed("ui_sprint")
 			if Input.is_action_just_released("ui_sprint"):
 				isSprinting = false
 			
