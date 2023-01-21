6c5dfc1bca73b58892d1b41da4212a349b1933f7
AGarciaNY <aldairgarcia360@gmail.com>
2023-01-21 15:00:32 -0500

    feat: made customers move to the right

--- customer/customer.gd
+++ customer/customer.gd
@@ -1,5 +1,7 @@
-extends RigidBody2D
+extends KinematicBody2D
 
+var velocity = Vector2(0,0)
+var speed = 100
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -10,12 +12,18 @@ extends RigidBody2D
 func _ready():
 	pass
 
-
+func _process(delta):
+	move_coustomer()
+	pass
+func move_coustomer():
+	velocity.x = speed
+	velocity.y -= 0
+	velocity = move_and_slide(velocity, Vector2.UP)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 
 func _on_VisibilityNotifier2D_screen_exited():
-	queue_free()
+	#queue_free()
 	pass # Replace with function body.
--- game/gameplay.gd
+++ game/gameplay.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var numberOfCustomer = 0
+
 export(PackedScene) var customer_node
 # Declare member variables here. Examples:
 # var a = 2
@@ -8,6 +10,7 @@ export(PackedScene) var customer_node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize()
 	$customerTimer.start()
 	pass # Replace with function body.
 
@@ -18,20 +21,18 @@ func _ready():
 
 
 func _on_customerTimer_timeout():
-	print("hi")
-	var mob = customer_node.instance()
-	
-	var mob_spawn_location = get_node("Path2D/MobSpawnLocation")
-	mob_spawn_location.offset = randi()
-	
-	var direction = mob_spawn_location.rotation + PI / 2
-	
-	mob.position = mob_spawn_location.position
-	
-	direction += rand_range( -PI/4, PI/4)
-	mob.rotation = direction
-	
-	var velocity = Vector2(rand_range(150,250), 0.0)
-	mob.linear_velocity = velocity.rotated(direction)
-	
-	add_child(mob)
+	if numberOfCustomer < 5:
+		print("numberOfCustomer",numberOfCustomer)
+		var mob = customer_node.instance()
+		
+		var mob_spawn_location = get_node("Path2D/MobSpawnLocation")
+		
+		mob_spawn_location.offset = randi() % 100
+		
+		mob.position = mob_spawn_location.position
+		
+		var velocity = Vector2(0, 200)
+		
+		
+		add_child(mob)
+		numberOfCustomer += 1
