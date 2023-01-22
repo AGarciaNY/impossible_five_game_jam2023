ab4433490c270d5039d0e5f102c43492ae5c05c9
Jason Alvarez <jasonalvarezcoraiza@gmail.com>
2023-01-21 20:40:04 -0500

    cooking interface init

--- kitchen/CookManager.gd
+++ kitchen/CookManager.gd
@@ -1,11 +1,16 @@
 extends Node2D
 onready var pantry = get_node("../CanvasLayer/PantrySelection")
+onready var stove = get_node("../CanvasLayer/Cook Interface")
 
 #Shows Pantry
 func _on_Area2D_area_entered(_area):
 	pantry.visible = true
 
 #hide Pantry
-func _on_Area2D_area_exited(area):
+func _on_Area2D_area_exited(_area):
 	pantry.visible = false
-	
+
+func _on_CookArea_area_entered(_area):
+	stove.populateInventory()
+	stove.visible = true
+	pass # Replace with function body.
